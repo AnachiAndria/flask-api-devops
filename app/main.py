fe8d0ee3b98f1ee05b5a27c77bc2e95ecc12018c
AnachiAndria <mamyachina@gmail.com>
2025-07-09 14:32:48 +0200

    test Depl

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Bienvenue dans l'API Flask !"})
+    return jsonify({"anachi said": "I love You mimisou!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
